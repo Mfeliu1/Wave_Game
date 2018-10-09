edf50ae0f1617a5d7d9b966a5cdf95657389e8a5
MJMurphy99 <mjmurphy6@quinnipiac.edu>
2018-10-09 19:09:22 -0400

    Lowered number of spaceships that bounce around the screen from 15 to 10

--- src/mainGame/ID.java
+++ src/mainGame/ID.java
@@ -16,7 +16,7 @@ package mainGame;
 public enum ID {
 	//How many of these should be spawned (will be changed + or - 20% unless marked as 1(for boss)
 	//-1 is a non-enemy entity
-	EnemyBasic(15),
+	EnemyBasic(10),
 	EnemyFast(15),
 	EnemySmart(10),
 	EnemyBurst(10),
