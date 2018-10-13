5a1efdc5ea1b7bcb4068fe996471ae76707579b2
Howard Byrd <mhscsp16.byrd@gmail.com>
2018-10-13 18:56:08 -0400

    Screen Changes based on screen.

--- src/mainGame/Game.java
+++ src/mainGame/Game.java
@@ -29,7 +29,7 @@ public class Game extends Canvas{
 	public static final Dimension canvasSize = new Dimension(1920, 1080);
 	public static final double aspectRatio   = (double)canvasSize.getWidth()/(double)canvasSize.getHeight();
 	static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	static int WindowWidth = (int) (0.9 * screenSize.getWidth());
+	static int WindowWidth = (int) (1.0 * screenSize.getWidth());
 	static int WindowHeight = (int) (0.9 *screenSize.getHeight());
 	public static final Dimension windowSize = new Dimension(WindowWidth,WindowHeight);
 	public static final int WIDTH  = (int)canvasSize.getWidth();
