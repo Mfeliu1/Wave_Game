fd1271088663e53c137d284b6695b7e284eabcc4
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2018-10-24 13:27:37 -0400

    fixed enemysmart going faster down and to the right

--- src/mainGame/EnemySmart.java
+++ src/mainGame/EnemySmart.java
@@ -4,6 +4,7 @@ package mainGame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.io.File;
 
@@ -45,19 +46,27 @@ public class EnemySmart extends Enemy {
 		this.y += velY;
 		////////////////////////////// pythagorean theorem
 		////////////////////////////// below//////////////////////////////////////////////////
+		/*
 		double diffX = this.x - player.getX() - 16;
 		double diffY = this.y - player.getY() - 16;
 		double distance = Math.sqrt(((this.x - player.getX()) * (this.x - player.getX()))
 				+ ((this.y - player.getY()) * (this.y - player.getY())));
+		*/
+		
 		////////////////////////////// pythagorean theorem
 		////////////////////////////// above//////////////////////////////////////////////////
-		velX = ((this.speed / distance) * diffX); // numerator affects speed of enemy
-		velY = ((this.speed / distance) * diffY);// numerator affects speed of enemy
+		//velX = ((this.speed / distance) * diffX); // numerator affects speed of enemy
+		//velY = ((this.speed / distance) * diffY);// numerator affects speed of enemy
 
 		// if (this.y <= 0 || this.y >= Game.HEIGHT - 40) velY *= -1;
 		// if (this.x <= 0 || this.x >= Game.WIDTH - 16) velX *= -1;
 
 		//handler.addObject(new Trail(x, y, ID.Trail, Color.green, 16, 16, 0.025, this.handler));
+		
+		double dir = pointDirection(new Point.Double(this.x,this.y),new Point.Double(player.x,player.y));
+		
+		velX = Math.cos(dir)*speed;
+		velY = Math.sin(dir)*speed;
 
 	}
 
@@ -71,5 +80,12 @@ public class EnemySmart extends Enemy {
 	public Rectangle getBounds() {
 		return new Rectangle((int) this.x-75, (int) this.y-30, 150, 75);
 	}
+	
+	public static double pointDirection(Point.Double p1, Point.Double p2)
+    {
+        double xDiff = p1.x - p2.x;
+        double yDiff = p1.y - p2.y;
+        return (Math.atan2(yDiff, xDiff));
+    }
 
 }
